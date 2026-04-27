2c6303a644dbddb36b93fc1caa719f3ca7ebc3e3
Thanh Vu <thanhvc.198@gmail.com>
2026-04-27 21:04:18 +0700

    fix(player): fix build ffmpeg

--- internal/infra/audio/cgoflags_darwin_amd64.go
+++ internal/infra/audio/cgoflags_darwin_amd64.go
@@ -5,7 +5,7 @@ package audio
 /*
 #cgo CFLAGS: -I${SRCDIR}/ffmpeg_libs/include
 #cgo LDFLAGS: -L${SRCDIR}/ffmpeg_libs/darwin/amd64
-#cgo LDFLAGS: -lavcodec -lavformat -lavutil -lswresample
+#cgo LDFLAGS: -lavformat -lavcodec -lswresample -lavutil
 #cgo LDFLAGS: -lz -lbz2 -liconv -llzma
 #cgo LDFLAGS: -framework CoreFoundation -framework Security
 #cgo LDFLAGS: -framework VideoToolbox -framework AudioToolbox -framework CoreMedia
--- internal/infra/audio/cgoflags_windows_amd64.go
+++ internal/infra/audio/cgoflags_windows_amd64.go
@@ -5,6 +5,6 @@ package audio
 /*
 #cgo CFLAGS: -I${SRCDIR}/ffmpeg_libs/include
 #cgo LDFLAGS: -L${SRCDIR}/ffmpeg_libs/windows/amd64
-#cgo LDFLAGS: -Wl,-Bstatic -lavcodec -lavformat -lavutil -lswresample -lz -lbz2 -liconv -llzma -Wl,-Bdynamic -lmfplat -lmf -lmfuuid -lstrmiids -lws2_32 -lsecur32 -lbcrypt -lole32 -loleaut32 -luuid -lwinmm -lversion -static-libgcc
+#cgo LDFLAGS: -Wl,-Bstatic -lavformat -lavcodec -lswresample -lavutil -lz -lbz2 -liconv -llzma -Wl,-Bdynamic -lmfplat -lmf -lmfuuid -lstrmiids -lws2_32 -lsecur32 -lbcrypt -lole32 -loleaut32 -luuid -lwinmm -lversion -static-libgcc
 */
 import "C"
